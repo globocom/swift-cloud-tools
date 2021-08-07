3d8e4c9c16180d73420204cc2711370af4f65ec4
Sergio Saules <sergios@corp.globo.com>
2021-08-07 15:10:20 -0300

    Fix: improved error handling

--- swift_cloud_tools/server/synchronize.py
+++ swift_cloud_tools/server/synchronize.py
@@ -9,7 +9,9 @@ from flask import Response
 from threading import Thread
 from swift_cloud_tools import create_app
 from google.cloud.exceptions import NotFound
+from google.auth.exceptions import TransportError
 from swiftclient import client as swift_client
+from keystoneauth1.exceptions.auth import AuthorizationFailure
 
 from swift_cloud_tools.server.utils import Keystone, Swift, Google, Transfer
 from swift_cloud_tools.models import TransferProject, TransferProjectError, db
@@ -87,7 +89,7 @@ class SynchronizeProjects():
             bucket.iam_configuration.uniform_bucket_level_access_enabled = False
             bucket.patch()
         except Exception as err:
-            self.app.logger.info('[{}] 500 GET Create bucket: {}'.format(
+            self.app.logger.error('[{}] 500 GET Create bucket: {}'.format(
                 transfer_object.project_name,
                 err
             ))
@@ -115,6 +117,11 @@ class SynchronizeProjects():
                     account_stat, containers = self.swift.get_account(marker=containers[len(containers) - 2].get('name'))
                 else:
                     account_stat, containers = self.swift.get_account()
+            except AuthorizationFailure:
+                app.logger.error("[{}] 500 GET account 'AUTH_{}': Keystone authorization failure".format(
+                    transfer_object.project_name,
+                    project_id
+                ))
             except Exception as err:
                 self.app.logger.info("[{}] {} GET account 'AUTH_{}': {}".format(
                     transfer_object.project_name,
@@ -173,8 +180,14 @@ class SynchronizeProjects():
             for count in pool.imap_unordered(
                     foo.send_container, self.iterator_slice(containers_copy, self.FLUSH_OBJECT)):
                 transfer.container_count_gcp += count
+
+                db.session.begin()
+                transfer_project = db.session.query(TransferProject).filter_by(project_id=transfer_object.project_id).first()
                 transfer_object.container_count_gcp = transfer.container_count_gcp
-                transfer_object.save()
+                time.sleep(0.1)
+                db.session.commit()
+
+                transfer_project = None
 
             pool.close()
 
@@ -185,6 +198,11 @@ class SynchronizeProjects():
                     self.conn = self.keystone.get_keystone_connection()
                     self.swift = Swift(self.conn, project_id)
                     account_stat, containers_copy = self.swift.get_account(marker=containers[-1].get('name'))
+                except AuthorizationFailure:
+                    app.logger.error("[{}] 500 Get account 'AUTH_{}': Keystone authorization failure".format(
+                        transfer_object.project_name,
+                        project_id
+                    ))
                 except Exception as err:
                     self.app.logger.info("[{}] {} GET account 'AUTH_{}': {}".format(
                         transfer_object.project_name,
@@ -204,11 +222,15 @@ class SynchronizeProjects():
 
         ########################################
 
-        percentage_page = float(os.environ.get("PERCENTAGE_PAGE", "0.1"))
-        page_size = int(int(container_count) * percentage_page) or 1
+        page_size = 1
+        parts = []
+
+        if len(containers) > 35:
+            percentage_page = float(os.environ.get("PERCENTAGE_PAGE", "0.1"))
+            page_size = int(int(container_count) * percentage_page) or 1
+
         start = 0
         end = page_size
-        parts = []
 
         while True:
             res = list(itertools.islice(containers, start, end))
@@ -221,11 +243,13 @@ class SynchronizeProjects():
         threads = [None] * len(parts)
         results = [None] * len(parts)
         for i in range(len(threads)):
-            threads[i] = Thread(target=self.mount_export_tree_async, args=(parts[i], bucket, transfer, transfer_object, resume, results, i))
+            time.sleep(0.5)
+            threads[i] = Thread(target=self.send_object, args=(parts[i], bucket, transfer, transfer_object, resume, results, i))
             threads[i].start()
         for i in range(len(threads)):
             threads[i].join()
         for result in results:
+            time.sleep(0.1)
             self.app.logger.info("[{}] Finished page container': {}".format(
                 transfer_object.project_name,
                 result
@@ -317,7 +341,7 @@ class SynchronizeProjects():
 
         ########################################
 
-    def mount_export_tree_async(self, containers, bucket, transfer, transfer_object, resume, result, index):
+    def send_object(self, containers, bucket, transfer, transfer_object, resume, result, index):
         app = create_app('config/{}_config.py'.format(os.environ.get("FLASK_CONFIG")))
         ctx = app.app_context()
         ctx.push()
@@ -344,9 +368,11 @@ class SynchronizeProjects():
                     if len(objects) == 0:
                         break
                 else:
+                    time.sleep(0.3)
                     meta, objects = self.swift.get_container(container.get('name'))
             except requests.exceptions.ConnectionError:
                 try:
+                    time.sleep(0.3)
                     self.conn = self.keystone.get_keystone_connection()
                     self.swift = Swift(self.conn, project_id)
                     if resume:
@@ -359,6 +385,16 @@ class SynchronizeProjects():
                             break
                     else:
                         meta, objects = self.swift.get_container(container.get('name'))
+                except AuthorizationFailure:
+                    if resume:
+                        path = '{}/{}'.format(container.get('name'), prefix)
+                    else:
+                        path = container.get('name')
+
+                    app.logger.error("[{}] 500 Get container '{}': Keystone authorization failure".format(
+                        transfer_object.project_name,
+                        path
+                    ))
                 except Exception as err:
                     if resume:
                         path = '{}/{}'.format(container.get('name'), prefix)
@@ -397,21 +433,22 @@ class SynchronizeProjects():
                     objects
                 )
 
-        app.logger.error("[{}] 201 Finished container '{}'".format(
+        app.logger.info("[{}] 201 Finished send_object '{}'".format(
             transfer_object.project_name,
-            container.get('name')
+            index
         ))
 
         db.session.begin()
-        transfer_project = TransferProject.query.filter_by(project_id=transfer_object.project_id).first()
+        transfer_project = db.session.query(TransferProject).filter_by(project_id=transfer_object.project_id).first()
         transfer_project.last_object = transfer.last_object
         transfer_project.count_error = transfer.count_error
         transfer_project.object_count_gcp = transfer.object_count_gcp
         transfer_project.bytes_used_gcp = transfer.bytes_used_gcp
+        time.sleep(0.1)
         db.session.commit()
 
+        transfer_project = None
         result[index] = 'ok'
-
 
     def _get_container(self, container, bucket, transfer_object, transfer, objects):
         for obj in objects:
@@ -440,19 +477,33 @@ class SynchronizeProjects():
                 transfer.last_object = '{}/{}'.format(container, prefix)
                 self.flush_object -= 1
 
-                if self.flush_object == 0:
-                    transfer_object.last_object = transfer.last_object
-                    transfer_object.save()
+                if self.flush_object <= 0:
+                    db.session.begin()
+                    transfer_project = db.session.query(TransferProject).filter_by(project_id=transfer_object.project_id).first()
+                    transfer_project.last_object = transfer.last_object
+                    transfer_project.count_error = transfer.count_error
+                    transfer_project.object_count_gcp = transfer.object_count_gcp
+                    transfer_project.bytes_used_gcp = transfer.bytes_used_gcp
+                    time.sleep(0.1)
+                    db.session.commit()
 
+                    transfer_project = None
                     self.flush_object = self.FLUSH_OBJECT
 
                 try:
+                    time.sleep(0.3)
                     meta, objects = self.swift.get_container(container, prefix=prefix)
                 except requests.exceptions.ConnectionError as err:
                     try:
+                        time.sleep(0.3)
                         self.conn = self.keystone.get_keystone_connection()
                         self.swift = Swift(self.conn, self.project_id)
                         meta, objects = self.swift.get_container(container, prefix=prefix)
+                    except AuthorizationFailure:
+                        app.logger.error("[{}] 500 Get container '{}': Keystone authorization failure".format(
+                            transfer_object.project_name,
+                            container
+                        ))
                     except Exception as err:
                         self.app.logger.error("[{}] {} Get container '{}/{}': {}".format(
                             transfer_object.project_name,
@@ -483,12 +534,20 @@ class SynchronizeProjects():
             else:
                 if obj.get('content_type') != 'application/directory':
                     try:
+                        time.sleep(0.3)
                         headers, content = self.swift.get_object(container, obj.get('name'))
                     except requests.exceptions.ConnectionError:
                         try:
+                            time.sleep(0.3)
                             self.conn = self.keystone.get_keystone_connection()
                             self.swift = Swift(self.conn, self.project_id)
                             headers, content = self.swift.get_object(container, obj.get('name'))
+                        except AuthorizationFailure:
+                            app.logger.error("[{}] 500 Get object '{}/{}': Keystone authorization failure".format(
+                                transfer_object.project_name,
+                                container,
+                                obj.get('name')
+                            ))
                         except Exception as err:
                             transfer.count_error += 1
                             obj_path = "{}/{}".format(container, obj.get('name'))
@@ -581,13 +640,17 @@ class SynchronizeProjects():
                     transfer.last_object = '{}/{}'.format(container, obj.get('name'))
                     self.flush_object -= 1
 
-                    if self.flush_object == 0:
-                        transfer_object.last_object = transfer.last_object
-                        transfer_object.count_error = transfer.count_error
-                        transfer_object.object_count_gcp = transfer.object_count_gcp
-                        transfer_object.bytes_used_gcp = transfer.bytes_used_gcp
-                        transfer_object.save()
+                    if self.flush_object <= 0:
+                        db.session.begin()
+                        transfer_project = db.session.query(TransferProject).filter_by(project_id=transfer_object.project_id).first()
+                        transfer_project.last_object = transfer.last_object
+                        transfer_project.count_error = transfer.count_error
+                        transfer_project.object_count_gcp = transfer.object_count_gcp
+                        transfer_project.bytes_used_gcp = transfer.bytes_used_gcp
+                        time.sleep(0.1)
+                        db.session.commit()
 
+                        transfer_project = None
                         self.flush_object = self.FLUSH_OBJECT
 
     def send_container(self, containers):
@@ -608,6 +671,19 @@ class SynchronizeProjects():
                 account,
                 timeout=30
             )
+        except TransportError:
+            try:
+                bucket = gcp_client.get_bucket(
+                    account,
+                    timeout=30
+                )
+            except Exception as err:
+                app.logger.error("[{}] {} Get bucket '{}': {}".format(
+                    self.project_name,
+                    err.http_status,
+                    account,
+                    err.http_reason
+                ))
         except requests.exceptions.ReadTimeout as err:
             try:
                 bucket = gcp_client.get_bucket(
@@ -621,16 +697,28 @@ class SynchronizeProjects():
                     account,
                     err.http_reason
                 ))
+        except Exception as err:
+            app.logger.error('[{}] 500 GET bucket: {}'.format(
+                self.project_name,
+                err
+            ))
 
         for container in containers:
             try:
+                time.sleep(0.4)
                 container_name = container.get('name')
                 meta, objects = self.swift.get_container(container_name)
             except requests.exceptions.ConnectionError:
                 try:
+                    time.sleep(0.4)
                     self.conn = self.keystone.get_keystone_connection()
                     self.swift = Swift(self.conn, self.project_id)
                     meta, objects = self.swift.get_container(container_name)
+                except AuthorizationFailure:
+                    app.logger.error("[{}] 500 Get container '{}': Keystone authorization failure".format(
+                        self.project_name,
+                        container_name
+                    ))
                 except Exception as err:
                     app.logger.error("[{}] {} Get container '{}': {}".format(
                         self.project_name,
