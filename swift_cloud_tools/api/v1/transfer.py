42ec86b2fa1d2dc22f82a56635b37c5ff7c99d31
Alan <avanselmo@gmail.com>
2022-02-23 17:51:14 -0300

    Rollback prior changes; update TransferItem endpoint

--- swift_cloud_tools/api/v1/transfer.py
+++ swift_cloud_tools/api/v1/transfer.py
@@ -65,10 +65,6 @@ class TransferItem(Resource):
             return {}, 404
 
         item = tp.to_dict()
-        if item.get('initial_date'):
-            item['initial_date'] = datetime.strftime(item.get('initial_date'), "%Y-%m-%d %H:%M:%S")
-        if item.get('final_date'):
-            item['final_date'] = datetime.strftime(item.get('final_date'), "%Y-%m-%d %H:%M:%S")
 
         return item, 200
 
@@ -80,11 +76,27 @@ class TransferStatus(Resource):
     def get(self, project_id):
         """Returns a project transfer item."""
 
+        status = None
         tp = TransferProject.find_transfer_project(project_id)
-        if not tp:
-            return {'message': 'Project not found'}, 404
 
-        return tp.to_dict()
+        if not tp:
+            return {'status': 'Migração não inicializada'}, 200
+
+        item = tp.to_dict()
+
+        if not item.get('initial_date') and not item.get('final_date'):
+            status = {'status': 'Aguardando migração'}
+
+        if item.get("initial_date") and not item.get("final_date"):
+            count_swift = item.get('object_count_swift')
+            count_gcp = item.get('count_error') + item.get('object_count_gcp')
+            progress = (100 * count_gcp) / count_swift
+            status = {'status': 'Migrando', 'progress': int(progress)}
+
+        if item.get("final_date"):
+            status = {'status': 'Migração concluída'}
+
+        return status, 200
 
 
 @ns.route('/status')
